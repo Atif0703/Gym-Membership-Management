bf271aaf7a0e371f644bfdfcc8e84a569e32e831
Atif Alam Ansari <101616231+Atif0703@users.noreply.github.com>
2024-06-11 21:26:12 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,20 @@
+import customtkinter as ctk
+import tkinter as tk
+from tkinter import messagebox, simpledialog, ttk
+from PIL import Image, ImageTk
+from tkcalendar import DateEntry
+import os
+import sqlite3
+import random
+import string
+import qrcode
+import cv2
+import datetime
+import requests
+import calendar
+import numpy as np
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import timedelta
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
